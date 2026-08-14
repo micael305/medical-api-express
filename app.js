9bbf0cd751e297ffae58c4a7d4cf6e2dda227f7d
micael305 <miicael305@gmail.com>
2026-08-13 21:37:24 -0300

    feat: add express body parsing middleware, dynamic routes, and query parameters

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
-console.log(PORT)
 
 app.get('/', (req, res) => {
     res.send(`
         <h1>ExpressJS</h1>
         <p>Esta es un aplicación node.js con express.js</p>
         <p>Corre en el puerto ${PORT}</p>`);
+})
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Mostrar información del usuario con ID: ${userId}`);
+})
+
+app.get('/search', (req, res) => {
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`
+        <h2>Resultados de busqueda</h2>
+        <p>Termino: ${terms}</p>
+        <p>Categoria: ${category}</p>
+        `);
 })
 
 app.listen(PORT, () => {
