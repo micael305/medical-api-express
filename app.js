618a5adaf5629017e9dfba0252ebf5edae329b50
micael305 <miicael305@gmail.com>
2026-08-17 16:38:49 -0300

    feat(auth): add JWT authentication middleware to protect routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import generalRoutes from './src/routes/general.routes.js';
 import usersRoutes from './src/routes/users.routes.js';
 import LoggerMiddleware from './src/middlewares/logger.js';
 import errorHandler from './src/middlewares/errorhandler.js';
+import authenticateToken from './src/middlewares/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(LoggerMiddleware);
 app.use(errorHandler);
+app.use(authenticateToken);
 
 app.use('/', generalRoutes);
 app.use('/users', usersRoutes);
